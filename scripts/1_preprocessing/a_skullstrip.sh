a3d31a7139a3d969fbd9a1bb64919f283f6cd605
Yuan Chang Leong <leong.yuanchang@gmail.com>
2021-07-02 10:23:44 -0500

    Add files via upload

--- scripts/1_preprocessing/a_skullstrip.sh
+++ scripts/1_preprocessing/a_skullstrip.sh
@@ -5,10 +5,15 @@
 # Set Parameters
 subjID=("1004" "1005" "1006" "1007" "1008" "1009" "1011" "1012" "1014" "1015" "1016" "1017" "1018" "1019" "1020" "1021" "1022" "1023" "1024" "1026" "1027" "1028" "1029" "1030" "1031" "1032" "1033" "1034" "1035" "1036" "1037" "1038" "1039" "1040" "1041" "1042" "1043" "1044")
 
-# bet doesn't seem to to work well with defaced data, the following subjects may require manual intervention if you use the defaced anatomicals
-# subjID=("1008" "1009" "1014" "1020" "1026")
+# The following subjects may require manual intervention (7/2/2021) edit:
+# "1008": -R -f 0.68 -g 0 -c 123 130 102
+# "1009": -R -f 0.37 -g 0 -c 125.0 135 99
+# "1014": -f 0.18 -g 0 -c 126 127 97
+# "1020": -S -f 0.20 -g 0 -c 125 136 105
+# "1024": -f 0.2 -g 0 -c 126 134 98
+# "1026": -R -f 0.30 -g 0.1 -c 126 134 101
 
-bid_dir='../../Polarization'
+bid_dir='../../../Polarization'
 anat_dir='../../data/fmri/anat_brain'
 
 for subjNo in "${subjID[@]}"
