21d95a07b73cc9f9722ca06ce47b924a1903b5b5
Yuan Chang Leong <leong.yuanchang@gmail.com>
2021-07-02 17:25:02 -0500

    Add files via upload

--- scripts/1_preprocessing/b_runPreproc.sh
+++ scripts/1_preprocessing/b_runPreproc.sh
@@ -2,12 +2,11 @@
 #export FSLPARALLEL=slurm
 
 # Subj Numbers
-# subjNo=("1004" "1005" "1006" "1007" "1008" "1009" "1011" "1012" "1014" "1015" "1016" "1017" "1018" "1019" "1020" "1021" "1022" "1023" "1024")
-# subjNo=("1026" "1027" "1028" "1029" "1030" "1031" "1032" "1033" "1034" "1035" "1036" "1037" "1038" "1039" "1040" "1041" "1042" "1043" "1044")
-
 subjNo=("1004" "1005" "1006" "1007" "1008" "1009" "1011" "1012" "1014" "1015" "1016" "1017" "1018" "1019" "1020" "1021" "1022" "1023" "1024" "1026" "1027" "1028" "1029" "1030" "1031" "1032" "1033" "1034" "1035" "1036" "1037" "1038" "1039" "1040" "1041" "1042" "1043" "1044")
 
 design="preproc"
+bids_dir="Users/ycleong/Desktop/Polarization"
+analysis_dir="Users/ycleong/Desktop/PolarizationAttn"
 
 mkdir 'fsfs/'$design
 
@@ -17,7 +16,7 @@ for subjID in "${subjNo[@]}"
 		for r in 1 2 3 4
 		    do
 
-        thisFile="../../Polarization/sub-$subjID/func/sub-${subjID}_task-run${r}_bold.nii.gz"
+        thisFile="/$bids_dir/sub-$subjID/func/sub-${subjID}_task-run${r}_bold.nii.gz"
         nvols=$(fslnvols $thisFile)
 
         \cp templates/preproc.fsf fsfs/$design/subj${subjID}_task_run${r}.fsf
@@ -28,10 +27,21 @@ for subjID in "${subjNo[@]}"
         sed -i -e 's/ChangeMyVolumes/'$nvols'/' fsfs/$design/subj${subjID}_task_run${r}.fsf  #Swap "ChangeMyRun" with run number
         sed -i -e 's/ChangeMyDesign/'$design'/' fsfs/$design/subj${subjID}_task_run${r}.fsf  #Swap "ChangeMyRun" with run number
 
+				sed -i -e 's@ChangeMyAnalysisDir@'${analysis_dir}'@' fsfs/$design/subj${subjID}_task_run${r}.fsf
+				sed -i -e 's@ChangeMyBidsDir@'${bids_dir}'@' fsfs/$design/subj${subjID}_task_run${r}.fsf
+
         \rm fsfs/$design/*-e #Remove excess schmutz
 
         echo Running Subj $subjID run $r
         feat fsfs/$design/subj${subjID}_task_run${r}.fsf
-        #sleep 3
-    done
+				done
+
+		for r in 1 2 3 4
+
+		do
+
+				# uncomment to remove the original data after running the model (to save disk space)
+				\rm /$bids_dir/sub-$subjID/func/sub-${subjID}_task-run${r}_bold.nii.gz
+
+	  done
 done
